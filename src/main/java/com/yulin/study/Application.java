6e491396d476838de4583ab64f1e2c61a4b30320
雨淋 <123456@qq.com>
2019-07-25 11:44:53 +0800

    包增加

--- src/main/java/com/yulin/study/Application.java
+++ src/main/java/com/yulin/study/Application.java
@@ -1,6 +1,5 @@
 package com.yulin.study;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -15,7 +14,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @EnableAsync
 @EnableTransactionManagement
-@MapperScan("com.yulin.study.mapper")
 @EnableScheduling
 @SpringBootApplication
 public class Application {
